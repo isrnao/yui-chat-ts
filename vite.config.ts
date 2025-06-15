8cdaf0a0b6caf8af9ba7459d57043da443d9868a
isrnao <jggpad@gmail.com>
2025-06-16 03:09:16 +0900

    fix: build

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import mdx from '@mdx-js/rollup';
 
 export default defineConfig({
+  base: './',
   plugins: [react(), mdx()],
   test: {
     environment: 'jsdom',
